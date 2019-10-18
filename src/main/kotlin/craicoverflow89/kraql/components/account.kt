577e412bbbf3ba74117f5aeb7f6afc318f592bba
CraicOverflow89 <craic.overflow@hotmail.com>
2019-10-18 16:23:47 +0100

    Sorted account permissions.

--- src/main/kotlin/craicoverflow89/kraql/components/account.kt
+++ src/main/kotlin/craicoverflow89/kraql/components/account.kt
@@ -3,13 +3,21 @@ package craicoverflow89.kraql.components
 class KraQLAccount(val database: KraQLDatabase, val name: String, val password: String, val permissions: HashMap<KraQLAccountPermission, Boolean> = hashMapOf()) {
 
     init {
-        permissions[KraQLAccountPermission.DATABASE_CREATE] = false
-        permissions[KraQLAccountPermission.DATABASE_DELETE] = false
-        permissions[KraQLAccountPermission.TABLE_CREATE] = false
-        permissions[KraQLAccountPermission.TABLE_DELETE] = false
+
+        // Default Permissions
+        KraQLAccountPermission.toOrderedList().forEach {
+            if(!permissions.containsKey(it)) permissions[it] = false
+        }
+
     }
 
     fun hasPermission(permission: KraQLAccountPermission) = permissions[permission]
+
+    fun permissionsToString() = ArrayList<String>().apply {
+        KraQLAccountPermission.toOrderedList().forEach {
+            add(if(permissions[it]!!) "+" else "-")
+        }
+    }.joinToString("")
 
     fun setPermission(permission: KraQLAccountPermission, value: Boolean) {
         permissions[permission] = value
@@ -18,9 +26,7 @@ class KraQLAccount(val database: KraQLDatabase, val name: String, val password: 
     fun toFile() = ""
     // NOTE: come back to this
 
-    override fun toString() = "{name: $name, database: ${database.name}, permissions:${permissions.map {
-        if(it.value) "+" else "-"
-    }.joinToString("")}}"
+    override fun toString() = "{name: $name, database: ${database.name}, permissions:${permissionsToString()}}"
 
     fun validatePassword(value: String): Boolean {
         return value == password
@@ -30,5 +36,16 @@ class KraQLAccount(val database: KraQLDatabase, val name: String, val password: 
 }
 
 enum class KraQLAccountPermission {
-    DATABASE_CREATE, DATABASE_DELETE, TABLE_CREATE, TABLE_DELETE
+    DATABASE_CREATE, DATABASE_DELETE, TABLE_CREATE, TABLE_DELETE;
+
+    companion object {
+
+        fun toOrderedList() = ArrayList<KraQLAccountPermission>().apply {
+            add(DATABASE_CREATE)
+            add(DATABASE_DELETE)
+            add(TABLE_CREATE)
+            add(TABLE_DELETE)
+        }
+
+    }
 }
