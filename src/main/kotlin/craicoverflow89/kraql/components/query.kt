88608d7b10a3af0aa02d1d08a137ef0fa960cc31
CraicOverflow89 <craic.overflow@hotmail.com>
2019-10-18 19:06:41 +0100

    Working on table insert query logic.

--- src/main/kotlin/craicoverflow89/kraql/components/query.kt
+++ src/main/kotlin/craicoverflow89/kraql/components/query.kt
@@ -1,11 +1,13 @@
 package craicoverflow89.kraql.components
 
-abstract class KraQLQuery(protected val tableName: String) {
+abstract class KraQLQuery(val tableName: String) {
 
     //
 }
 
 class KraQLQueryInsert(tableName: String, private val fieldList: ArrayList<String>, private val recordList: ArrayList<String>): KraQLQuery(tableName) {
+
+    // NOTE: so here we need to create a public val data HashMap of fields to records
 
     override fun toString() = "{table: $tableName, fieldList: $fieldList, recordList: $recordList}"
 }
--- src/main/kotlin/craicoverflow89/kraql/testQueryParser.kt
+++ src/main/kotlin/craicoverflow89/kraql/testQueryParser.kt
@@ -1,17 +1,30 @@
 package craicoverflow89.kraql
 
+import craicoverflow89.kraql.queries.KraQLQueryLexer
+import craicoverflow89.kraql.queries.KraQLQueryParser
+import org.antlr.v4.runtime.ANTLRInputStream
+import org.antlr.v4.runtime.CommonTokenStream
+
 fun main() {
 
     // Load Database
     val db = KraQLApplication.loadDatabase("C:/Users/jamie/Software/Kotlin/KraQL/data/test.kqld")
 
-    // Execute Query
-    println(db.query("""
+    // Test Query
+    val input = """
         INSERT INTO test (name)
         VALUES ('James')
-    """))
+    """
+
+    // Parse Query
+    val lexer = KraQLQueryLexer(ANTLRInputStream(input))
+    val parser = KraQLQueryParser(CommonTokenStream(lexer))
+    val query = parser.query().result)
+
+    // Add Record
+    db.getTable(query.tableName).addRecord(query.data)
 
     // Debug Table
-    // NOTE: need database get table for this
+    println(db.getTable(query.tableName))
 
 }
