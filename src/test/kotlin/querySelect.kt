ab260548f0b5998aabd884c23c13d2612d22985b
CraicOverflow89 <craic.overflow@hotmail.com>
2019-10-29 09:48:12 +0000

    Added more tests to query select suite for specific fields, where clause and order by.

--- src/test/kotlin/querySelect.kt
+++ src/test/kotlin/querySelect.kt
@@ -55,7 +55,100 @@ class KraQLQuerySelectTest {
     }
 
     @Test
-    fun valid() {
+    fun orderByNameAscending() {
+
+        // Load Database
+        val db = KraQLApplication.loadDatabase("C:/Users/jamie/Software/Kotlin/KraQL/data/test.kqld"). apply {
+            setDebugSaveIgnore(true)
+        }
+
+        // Select Records
+        val result = db.query("""
+            SELECT *
+            FROM test
+            ORDER BY name ASC
+        """)
+
+        // Name Values
+        result.getData()!!.getRecords().let {
+            Assert.assertEquals("James", it[0].data["name"])
+            Assert.assertEquals("Josh", it[1].data["name"])
+        }
+
+        // Select Count
+        Assert.assertEquals(2, result.getCount())
+    }
+
+    @Test
+    fun orderByNameDescending() {
+
+        // Load Database
+        val db = KraQLApplication.loadDatabase("C:/Users/jamie/Software/Kotlin/KraQL/data/test.kqld"). apply {
+            setDebugSaveIgnore(true)
+        }
+
+        // Select Records
+        val result = db.query("""
+            SELECT *
+            FROM test
+            ORDER BY name DESC
+        """)
+
+        // Name Values
+        result.getData()!!.getRecords().let {
+            Assert.assertEquals("Josh", it[0].data["name"])
+            Assert.assertEquals("James", it[1].data["name"])
+        }
+
+        // Select Count
+        Assert.assertEquals(2, result.getCount())
+    }
+
+    @Test
+    fun selectAll() {
+
+        // Load Database
+        val db = KraQLApplication.loadDatabase("C:/Users/jamie/Software/Kotlin/KraQL/data/test.kqld"). apply {
+            setDebugSaveIgnore(true)
+        }
+
+        // Select Records
+        val result = db.query("""
+            SELECT *
+            FROM test
+        """)
+
+        // Data Fields
+        result.getData()!!.getRecords()[0].data.apply {
+            Assert.assertEquals(true, this.containsKey("name"))
+            Assert.assertEquals(true, this.containsKey("dob"))
+        }
+        // NOTE: would be even better to check table fieldList all exist in data keys
+    }
+
+    @Test
+    fun selectMultiple() {
+
+        // Load Database
+        val db = KraQLApplication.loadDatabase("C:/Users/jamie/Software/Kotlin/KraQL/data/test.kqld"). apply {
+            setDebugSaveIgnore(true)
+        }
+
+        // Select Records
+        val result = db.query("""
+            SELECT name, dob
+            FROM test
+        """)
+
+        // Data Fields
+        result.getData()!!.getRecords()[0].data.apply {
+            Assert.assertEquals(true, this.containsKey("name"))
+            Assert.assertEquals(true, this.containsKey("dob"))
+        }
+    }
+
+    @Test
+    fun selectName() {
 
         // Load Database
         val db = KraQLApplication.loadDatabase("C:/Users/jamie/Software/Kotlin/KraQL/data/test.kqld"). apply {
@@ -66,12 +159,79 @@ class KraQLQuerySelectTest {
         val result = db.query("""
             SELECT name
             FROM test
+        """)
+
+        // Data Fields
+        result.getData()!!.getRecords()[0].data.apply {
+            Assert.assertEquals(true, this.containsKey("name"))
+            Assert.assertEquals(false, this.containsKey("dob"))
+        }
+    }
+
+    @Test
+    fun whereEquals() {
+
+        // Load Database
+        val db = KraQLApplication.loadDatabase("C:/Users/jamie/Software/Kotlin/KraQL/data/test.kqld"). apply {
+            setDebugSaveIgnore(true)
+        }
+
+        // Select Records
+        val result = db.query("""
+            SELECT *
+            FROM test
+            WHERE name = 'James'
+        """)
+
+        // Name Values
+        Assert.assertEquals("James", result.getData()!!.getRecords()[0].data["name"])
+
+        // Select Count
+        Assert.assertEquals(1, result.getCount())
+    }
+
+    @Test
+    fun whereLike() {
+
+        // Load Database
+        val db = KraQLApplication.loadDatabase("C:/Users/jamie/Software/Kotlin/KraQL/data/test.kqld"). apply {
+            setDebugSaveIgnore(true)
+        }
+
+        // Select Records
+        val result = db.query("""
+            SELECT *
+            FROM test
             WHERE name LIKE 'J%'
         """)
 
         // Name Values
         Assert.assertEquals("James", result.getData()!!.getRecords()[0].data["name"])
         Assert.assertEquals("Josh", result.getData()!!.getRecords()[1].data["name"])
+
+        // Select Count
+        Assert.assertEquals(2, result.getCount())
+    }
+
+    @Test
+    fun whereNone() {
+
+        // Load Database
+        val db = KraQLApplication.loadDatabase("C:/Users/jamie/Software/Kotlin/KraQL/data/test.kqld"). apply {
+            setDebugSaveIgnore(true)
+        }
+
+        // Select Records
+        val result = db.query("""
+            SELECT *
+            FROM test
+        """)
+
+        // Name Values
+        result.getData()!!.getRecords().let {
+            Assert.assertEquals("James", it[0].data["name"])
+            Assert.assertEquals("Josh", it[1].data["name"])
+        }
 
         // Select Count
         Assert.assertEquals(2, result.getCount())
